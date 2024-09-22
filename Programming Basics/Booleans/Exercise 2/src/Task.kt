e540751c646fd02f77e071fcd5cc61077f676eb4
CarefreeLife <csm4903@naver.com>
2024-09-22 19:27:33 +0900

    24.09.22 학습 내용 추가

--- Programming Basics/Booleans/Exercise 2/src/Task.kt
+++ Programming Basics/Booleans/Exercise 2/src/Task.kt
@@ -1,12 +1,26 @@
 // Booleans/Task2.kt
 package booleansExercise2
 
-fun showAnd() {}
+fun showAnd(first: Boolean, second: Boolean){
+  println("$first && $second == ${first && second}")
+}
 
-fun showOr() {}
+fun showOr(first: Boolean, second: Boolean){
+  println("$first || $second == ${first || second}")
+}
 
 fun showTruthTable() {
-  TODO()
+  val table:Array<Array<Boolean>> = arrayOf(
+    arrayOf(true, true),
+    arrayOf(true, false),
+    arrayOf(false, true),
+    arrayOf(false, false),
+  )
+
+  for (row in table) {
+    showAnd(row[0], row[1])
+    showOr(row[0], row[1])
+  }
 }
 
 fun main() {
